05ac1a47cf68c54c1ae99e69602159674ba9a47a
orchistro <orchistro@gmail.com>
2021-03-29 18:33:18 +0900

    almost completed testing code

--- foo.cpp
+++ foo.cpp
@@ -4,14 +4,16 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdint.h>
 
-#include <cstdint>
 #include <vector>
 #include <memory>
 #include <string>
 #include <exception>
 #include <functional>
 #include <iostream>
+#include <chrono>
+#include <locale>
 
 #if !defined(__clang__) && defined(__GNUC__)
 /* Prototypes for our hooks.  */
@@ -94,7 +96,88 @@ void my_final(void)
 #endif
 }
 
-std::string makeString_vasprintf(const char *aFormat, ...)
+static const char* get_cache_type_str(const int32_t type)
+{
+    switch (type)
+    {
+        case 1:
+            return "Data Cache";
+        case 2:
+            return "Instruction Cache";
+        case 3:
+            return "Unified Cache";
+        default:
+            return "Unknown Type Cache";
+    }
+}
+
+void i386_cpuid_caches (void)
+{
+    int i;
+    for (i = 0; i < 32; i++) {
+
+        // Variables to hold the contents of the 4 i386 legacy registers
+        uint32_t eax, ebx, ecx, edx;
+
+        eax = 4; // get cache info
+        ecx = i; // cache id
+
+        __asm__ (
+            "cpuid" // call i386 cpuid instruction
+            : "+a" (eax) // contains the cpuid command code, 4 for cache query
+            , "=b" (ebx)
+            , "+c" (ecx) // contains the cache id
+            , "=d" (edx)
+            ); // generates output in 4 registers eax, ebx, ecx and edx
+
+        // See the page 3-191 of the manual.
+        int cache_type = eax & 0x1F;
+
+        if (cache_type == 0) // end of valid cache identifiers
+            break;
+
+        int cache_level = (eax >>= 5) & 0x7;
+
+        int cache_is_self_initializing = (eax >>= 3) & 0x1; // does not need SW initialization
+        int cache_is_fully_associative = (eax >>= 1) & 0x1;
+
+        // See the page 3-192 of the manual.
+        // ebx contains 3 integers of 10, 10 and 12 bits respectively
+        unsigned int cache_sets = ecx + 1;
+        unsigned int cache_coherency_line_size = (ebx & 0xFFF) + 1;
+        unsigned int cache_physical_line_partitions = ((ebx >>= 12) & 0x3FF) + 1;
+        unsigned int cache_ways_of_associativity = ((ebx >>= 10) & 0x3FF) + 1;
+
+        // Total cache size is the product
+        size_t cache_total_size = cache_ways_of_associativity * cache_physical_line_partitions * cache_coherency_line_size * cache_sets;
+
+        printf(
+            "Cache ID %d:\n"
+            "- Level: %d\n"
+            "- Type: %s\n"
+            "- Sets: %d\n"
+            "- System Coherency Line Size: %d bytes\n"
+            "- Physical Line partitions: %d\n"
+            "- Ways of associativity: %d\n"
+            "- Total Size: %zu bytes (%zu kb)\n"
+            "- Is fully associative: %s\n"
+            "- Is Self Initializing: %s\n"
+            "\n"
+            , i
+            , cache_level
+            , get_cache_type_str(cache_type)
+            , cache_sets
+            , cache_coherency_line_size
+            , cache_physical_line_partitions
+            , cache_ways_of_associativity
+            , cache_total_size, cache_total_size >> 10
+            , cache_is_fully_associative ? "true" : "false"
+            , cache_is_self_initializing ? "true" : "false"
+            );
+    }
+}
+
+static std::string makeString_vasprintf(const char *aFormat, ...)
 {
     int32_t sLen;
     va_list sAp;
@@ -122,7 +205,7 @@ std::string makeString_vasprintf(const char *aFormat, ...)
     }
 }
 
-std::string makeString_vsnprintf(const char *aFormat, ...)
+static std::string makeString_vsnprintf(const char *aFormat, ...)
 {
     int32_t sLen;
     va_list sAp;
@@ -180,28 +263,63 @@ static std::vector<std::string> gen_strings(const int32_t count, const int32_t m
     return result;
 }
 
-std::int32_t main(std::int32_t argc, char* argv[])
+typedef std::string method_func(const char *aFormat, ...);
+
+static void test(method_func* method, const char* name, const std::vector<std::string>& vec)
 {
-    std::srand(std::time(0));
+    std::cout << "Testing with method " << name;
 
-    // if (argc != 4)
-    // {
-    //     std::cerr << "Usage: ./foo count min_lenght max_length\n";
-    //     return 1;
-    // }
-
-    std::int32_t count = 1000;
-    std::int32_t min_len = 100;
-    std::int32_t max_len = 1000;
-
-    std::vector<std::string> vec = gen_strings(count, min_len, max_len);
+    auto start_time = std::chrono::steady_clock::now();
 
     for (const auto& str : vec)
     {
-        std::cout << str.length() << "  ";
-        std::string s = makeString_vsnprintf(str.c_str());
-        std::cout << s.length() << "\n";
-        // std::cout << s << "\n";
+        std::cerr << str.length() << "  ";
+        std::string s = method(str.c_str());
+        std::cerr << s.length() << "\n";
+    }
+
+    auto end_time = std::chrono::steady_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+
+    uint64_t elapsed_int = elapsed.count();
+    std::cout << "    --> Took " << std::fixed << elapsed_int << "us ";
+    std::cout << "(" << elapsed_int / 1000 << "ms)\n";
+}
+
+int32_t main(int32_t argc, char* argv[])
+{
+    std::cout.imbue(std::locale(""));
+
+    std::srand(std::time(0));
+
+    if (strcmp(argv[1], "cpuinfo") == 0)
+    {
+        i386_cpuid_caches();
+        return 0;
+    }
+
+    if (argc != 4)
+    {
+        std::cerr << "Usage: ./foo count min_lenght max_length\n";
+        return -1;
+    }
+
+    int32_t count = std::atoi(argv[1]);
+    int32_t min_len = std::atoi(argv[2]);
+    int32_t max_len = std::atoi(argv[3]);
+
+    if (min_len > max_len)
+    {
+        std::swap(min_len, max_len);
+    }
+
+    std::cout << "Generating " << count << " strings of length " << min_len << " ~ " << max_len << "\n";
+    std::vector<std::string> vec = gen_strings(count, min_len, max_len);
+
+    for (int32_t i = 0; i < 4; i++)
+    {
+        test(makeString_vasprintf, "vasprintf", vec);
+        test(makeString_vsnprintf, "vsnprintf", vec);
     }
 
     return 0;
